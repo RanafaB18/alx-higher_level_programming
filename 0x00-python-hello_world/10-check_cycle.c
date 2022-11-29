83969564dd44de80fe6c070576e112f1b349a783
RanafaB18 <raziqbaba321@gmail.com>
2022-11-29 01:58:36 +0000

    Ex 100

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,16 +10,19 @@ int check_cycle(listint_t *list)
 	listint_t *prev = list;
 	listint_t *next = list;
 	
+	printf("Entering check\n");
 	if (!list)
 	{
 		return (0);
 	}
 	while (next != NULL && next->next != NULL && prev != NULL)
 	{
+		printf("Before change: prev: %d ==== next: %d\n", prev->n, next->n);
 		prev = prev->next;
 		next = (next->next)->next;
 		if (next == prev)
 		{
+			printf("After change: prev: %d %p  ==== next: %p\n", prev->n, (void *) prev, (void     *) next);
 			return (1);
 		}
 	}
