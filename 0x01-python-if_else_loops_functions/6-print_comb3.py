aa5efb9af08f5213fb4b436062c4e8b61fda2684
RanafaB18 <raziqbaba321@gmail.com>
2022-11-29 18:38:54 +0000

    Ex 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-combos = []
-for i in range(0, 10):
+for i in range(0, 9):
 	for j in range(1, 10):
 		if j <= i:
 			continue
-		combos.append(str(i) + str(j))
-print("{}".format(*combos), sep=", ", end="")
+		print("{}{}".format(i, j), end="")
+		if i != 8:
+			print(", ", end="")
 print()
