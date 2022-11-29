2b9a221f4c6cc5c2a292b5262548e481ba439140
RanafaB18 <raziqbaba321@gmail.com>
2022-11-29 00:30:55 +0000

    Ex 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* check_cycle - Check if the linked list is a circular linked list
+* @list: linked list
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list;
+	listint_t *temp = list;
+
+	while (temp->next != NULL)
+	{
+		if (temp->next == head)
+		{
+			return (1);
+		}
+		temp = temp->next;
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/mymain.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+#include <stdio.h>
+
+int main(void)
+{
+	check_cycle(
+	return (0);
+}
