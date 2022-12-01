f1c83764ead19c3ad836096c354e17d3cc23ae7b
RanafaB18 <raziqbaba321@gmail.com>
2022-12-01 23:27:22 +0000

    Ex 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from sys import argv
 
     numOfArgs = len(argv)
-    if numOfArgs != 3:
+    if numOfArgs != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     operators = "+-*/"
