bd3c3fac552463ac28e5c75c0bd39bfb974db2f3
RanafaB18 <raziqbaba321@gmail.com>
2022-12-04 23:15:52 +0000

    Ex 100

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,43 +1,71 @@
 #include "lists.h"
+
 /**
- * is_palindrome - Checks if a linked list is palindromic
- * @head: head oflinkedlist
- * Return: 1 if palindromic else 0
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head;
-	int *var = NULL;
-	int i = 1;
-	int j;
-	int arrayLength;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	var = malloc(sizeof(int));
-	if (!head)
-	{
-		free(var);
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	}
-	while (temp)
-	{
-		var[i - 1] = temp->n;
-		i++;
-		var = realloc(var, sizeof(int) * i);
-		temp = temp->next;
-	}
 
-	arrayLength = i - 1;
-	i = 0;
-	for (j = arrayLength - 1; j > 0; j--)
+	while (1)
 	{
-		if (var[i] != var[j])
+		fast = fast->next->next;
+		if (!fast)
 		{
-			free(var);
-			return (0);
+			dup = slow->next;
+			break;
 		}
-		i++;
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	free(var);
-	return (1);
-}
 
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
+}
