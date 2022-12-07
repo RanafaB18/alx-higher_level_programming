77662cd42a757d6a16b5f2462370415a287807df
RanafaB18 <raziqbaba321@gmail.com>
2022-12-07 22:10:48 +0000

    Ex 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     length = len(my_list)
+    new_list = []
     if length > 0:
-        new_list = []
         for num in my_list:
             if num == search:
                 new_list.append(replace)
