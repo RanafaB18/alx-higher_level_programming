8cc28c91cf8f0412078583d4afe9da0a4635e57a
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 14:36:15 +0000

    Ex 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - print all elements of a doubly linked list
+ * @h: head of doublylinked list
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp = h;
