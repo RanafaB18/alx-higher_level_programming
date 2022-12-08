c658793a3bf949906ca046c1aa97ef22a9069cbe
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 02:43:32 +0000

    Ex 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        maximum = 0
+        for key, value in a_dictionary.items():
+            if value > maximum:
+                maximum = value
+                max_key = key
+    else:
+	 return None
+    return max_key
