bdc408913dd65e02c22df8417e38ca874a881e7b
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2022-12-19 23:30:49 +0000

    Ex 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        raise ValueError
+    except (ValueError, TypeError):
         return False
     else:
         return True
