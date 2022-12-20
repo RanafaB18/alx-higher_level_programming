71c3c7157498ae5bddc4feb1b0576e55ee44529b
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2022-12-20 01:10:12 +0000

    Ex 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,6 @@ def safe_print_list_integers(my_list=[], x=0):
         count = 0
         for i in range(x):
             try:
-                if (type(my_list[i]) is not int):
-                    continue
                 print("{:d}".format(my_list[i]), end="")
                 count = count + 1
             except (ValueError, TypeError):
