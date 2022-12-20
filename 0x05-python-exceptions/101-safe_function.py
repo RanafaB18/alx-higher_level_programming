0a69a4d1264b3abaaa56d4d7a0ca37b954f3216c
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2022-12-20 04:00:19 +0000

    Ex 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(args[0], args[1])
+    except ZeroDivisionError as err:
+        print("Exception: {}".format(err.args[0]), file=sys.stderr)
+    except IndexError as err:
+        print("Exception: {}".format(err.args[0]), file=sys.stderr)
