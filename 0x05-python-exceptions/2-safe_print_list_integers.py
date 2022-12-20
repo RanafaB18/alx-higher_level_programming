3edc7059777bdf2921ebfa69a5d89de924bb2397
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2022-12-20 01:05:23 +0000

    Ex 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+        count = 0
+        for i in range(x):
+            try:
+                if (type(my_list[i]) is not int):
+                    continue
+                print("{:d}".format(my_list[i]), end="")
+                count = count + 1
+            except ValueError:
+                raise ValueError
+            except TypeError:
+                raise TypeError
+        print()
+        return count
