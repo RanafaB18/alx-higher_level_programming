04e52e7db62837f3a3bab78eb4326ed90ff65740
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2022-12-21 03:18:38 +0000

    Ex 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Create a square"""
+
+    def __init__(self, size=0) -> None:
+        """Setup square
+
+        Args:
+            self: object
+            size: size of square
+
+        Returns:
+            None
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """size getter function
+
+        Args:
+            self: object
+
+        Returns:
+            size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter function
+
+        Args:
+            self: object
+            value: new value for size
+
+        Returns:
+            None
+
+
+        Raises:
+            TypeError: If size isn't an int
+            ValueError: if size is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate area of the square
+
+        Args:
+            self: object
+
+        Returns:
+            size squared
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints the square
+
+        Args:
+            self: object
+
+        Returns:
+            None
+        """
+        if self.size == 0:
+            print()
+        else:
+            for height in range(self.size):
+                for width in range(self.size):
+                    print("#", end="")
+                print()
