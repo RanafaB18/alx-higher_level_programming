e97b77fe04e736f0fb65d05f6cae84a74e702349
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-03 01:06:50 +0000

    Ex 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,18 +5,19 @@ Adds 2 integers with the add_integer function
 
 """
 
+
 def add_integer(a, b=98):
-    """
+    """Function that adds two integer and/or float numbers
+
     Args:
-        a (_type_): first integer
+        a (int): first integer
         b (int, optional): second integer. Defaults to 98.
 
     Raises:
-        TypeError: if a is not an int or float
-        TypeError: if b is not an int or float
+        TypeError: if a or b is not an int or float
 
     Returns:
-        _type_: addition of a and b
+        int: addition of a and b
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
