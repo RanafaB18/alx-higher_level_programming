a254ed8e199c51127f0f2303c9f2a6791f8595bc
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-09 23:59:34 +0000

    Ex 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function "inherits_from" """
+
+
+def inherits_from(obj, a_class):
+    """Checks if the obj is an instance of a_class
+
+    Args:
+        obj (object): object
+        a_class (object): class
+
+    Returns:
+        bool: True if obj is an instance of a class
+        that inherited from from a_class else False
+    """
+    return issubclass(type(obj), a_class)
