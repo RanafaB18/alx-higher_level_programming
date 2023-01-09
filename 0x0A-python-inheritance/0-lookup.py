96a4356b4bc274be21d79b141536acbb836f1ee3
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-09 18:54:39 +0000

    Ex 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """ Returns the list of available attributes and methods of an object
+
+    Args:
+        obj (object): instance of some class
+
+    Returns:
+        list: list of available attributes and methods of an object:
+    """
+    return dir(obj)
