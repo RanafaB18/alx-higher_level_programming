5ac9f45da487c5b269d2ef20ac8d94ef951d0522
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-09 19:29:37 +0000

    Ex 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines a class "MyList" """
+
+
+class MyList(list):
+    """Replica of the list datatype"""
+
+    def __init__(self):
+        """Initializes the class
+        """
+        list.__init__(self)
+
+    def print_sorted(self):
+        """Prints a sorted list"""
+        self.sort()
+        print(self)
