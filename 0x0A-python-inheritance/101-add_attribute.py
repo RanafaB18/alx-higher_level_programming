58807d5f4ea34e6d0f2259f65d747f0c1eb625ff
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-10 03:14:03 +0000

    Ex 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """Defines the add_attribute function"""
 
+
 def add_attribute(obj, key, value):
+    """Adds new attribute to obj if possible
+
+    Args:
+        obj (object): object
+        key (str): key
+        value (any): value
+
+    Raises:
+        TypeError: if it can't add a new attribute
+    """
     types = [
         int,
         float,
