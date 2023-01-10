ccb5b02fc670327208ceff785c213ef2e6e2803f
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-10 00:40:51 +0000

    Ex 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the class "BaseGeometry" """
+
+
+class BaseGeometry:
+    """Defines the instance method "area" """
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
