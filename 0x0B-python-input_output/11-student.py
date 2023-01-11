02022b2231e7499b483693144924507331938fb8
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-11 03:50:34 +0000

    Ex 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -21,5 +21,6 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        self.__dict__ = json
-        return self.__dict__
+        """replaces all attributes of the Student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)
