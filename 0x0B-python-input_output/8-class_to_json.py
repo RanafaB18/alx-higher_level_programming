e4a2ed331e258495b5a29ad81d07a38375ca89ca
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-11 03:05:01 +0000

    Ex 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the function 'class_to_json'"""
 
+
 def class_to_json(obj):
     """ returns the dictionary description with
     simple data structure (list, dictionary, string,
