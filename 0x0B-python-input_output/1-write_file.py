107e6b3d4ef60469a5fc76d4e02f9271230d9902
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-11 01:32:56 +0000

    Ex 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,15 @@
 
 
 def write_file(filename="", text=""):
+    """Writes to a file
+
+    Args:
+        filename (str, optional): filename. Defaults to "".
+        text (str, optional): Text to be written. Defaults to "".
+
+    Returns:
+        int: number of characters written to file
+    """
     with open(filename, 'w', encoding='utf-8') as file:
         num_chars = file.write(text)
     return num_chars
