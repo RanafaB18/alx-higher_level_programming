55a76471cfb94027f24f60c36778299b55c6c22e
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-11 01:32:07 +0000

    Ex 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function 'write_file'"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as file:
+        num_chars = file.write(text)
+    return num_chars
