2e924e2ea17d3d7145ce51f9d69fb223c33ffc25
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-03-14 16:26:04 +0000

    Ex 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-first = Number(process.argv[2]);
-const output =  isNaN(first) ? 'Not a number' : `My number: ${Math.floor(first)}`;
-console.log(output)
+const first = Number(process.argv[2]);
+const output = isNaN(first) ? 'Not a number' : `My number: ${Math.floor(first)}`;
+console.log(output);
