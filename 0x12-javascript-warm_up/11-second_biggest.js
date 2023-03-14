f5be8e7b239f3878f69621f75eb4870d5cfd53a0
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-03-14 17:16:25 +0000

    Ex 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,16 @@
 
 const numOfArgs = process.argv.length;
 if (numOfArgs === 2 || numOfArgs === 3) {
-  console.log(1);
+  console.log(0);
 } else {
   let maximum = Number(process.argv[2]);
+  let prevMaximum = 0
   for (let index = 3; index < numOfArgs; index++) {
     const current = Number(process.argv[index]);
     if (current > maximum) {
+      prevMaximum = maximum
       maximum = current;
     }
   }
-  console.log(maximum);
+  console.log(prevMaximum);
 }
