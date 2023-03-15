c675230c05c37f6db19c173617a847ee7ad38ab0
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-03-15 02:34:04 +0000

    Ex 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const len = list.length
-    const reversedArray = []
-    for (let i = len - 1; i >= 0; i--){
-        reversedArray[(len-1) - i] = list[i]
-    }
-    return reversedArray
-}
+  const len = list.length;
+  const reversedArray = [];
+  for (let i = len - 1; i >= 0; i--) {
+    reversedArray[(len - 1) - i] = list[i];
+  }
+  return reversedArray;
+};
